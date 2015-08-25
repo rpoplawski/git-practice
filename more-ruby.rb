cbd44ed0fe9e4d56082b755797932d932bc88d3e
Ryan Poplawski <claydewd@gmail.com>
2015-08-25 12:01:45 -0500

    second commit

--- more-ruby.rb
+++ more-ruby.rb
@@ -19,20 +19,22 @@ puts multiply(10, 20)
 puts multiply(10, 5)
 
 puts "welcome to square -ulator"
-puts "Would youlike to (D)ouble a number or (S)quare a number?"
-print "(D or S) > "
-response = gets.chomp.upcase
-if response == "D"
-  print "What number do you want to double? > "
-  response_number =gets.chomp.to_i
-  puts "Double of #{response_number} is #{double(response_number)}"
-elsif response == "S"
-print "what number do you want to square? > "
-response_number = gets.chomp.to_i
-puts "square of #{response_number} is #{square(response_number)}"
-elsif response == "Q"
-  puts "Goodbye!"
-  exit
-else
-  puts "What?"
-end
+loop do
+  puts "Would you like to (D)ouble a number or (S)quare a number, or (Q)uit ?"
+  print "(D or S) > "
+  response = gets.chomp.upcase
+  if response == "D"
+    print "What number do you want to double? > "
+    response_number =gets.chomp.to_i
+    puts "Double of #{response_number} is #{double(response_number)}"
+  elsif response == "S"
+  print "what number do you want to square? > "
+  response_number = gets.chomp.to_i
+  puts "square of #{response_number} is #{square(response_number)}"
+  elsif response == "Q"
+    puts "Goodbye!"
+    exit
+  else
+    puts "What?"
+  end #end of if
+end #end of loop
