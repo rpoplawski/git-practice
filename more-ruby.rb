9ec13307b72ead7ba7db64b3c59940277a547768
Ryan Poplawski <claydewd@gmail.com>
2015-08-25 11:53:46 -0500

    first commit

---
+++ more-ruby.rb
@@ -0,0 +1,38 @@
+a = 10
+b = 2
+
+a * b
+a * b
+def multiply(first, last) #arity of 2
+  first * last
+end
+def double(x)
+x * 2
+end
+
+def square(x)
+x * x
+end
+#arity; (number of arguments)
+system('clear') #the system methos runs any string you give it on the command line
+puts multiply(10, 20)
+puts multiply(10, 5)
+
+puts "welcome to square -ulator"
+puts "Would youlike to (D)ouble a number or (S)quare a number?"
+print "(D or S) > "
+response = gets.chomp.upcase
+if response == "D"
+  print "What number do you want to double? > "
+  response_number =gets.chomp.to_i
+  puts "Double of #{response_number} is #{double(response_number)}"
+elsif response == "S"
+print "what number do you want to square? > "
+response_number = gets.chomp.to_i
+puts "square of #{response_number} is #{square(response_number)}"
+elsif response == "Q"
+  puts "Goodbye!"
+  exit
+else
+  puts "What?"
+end
